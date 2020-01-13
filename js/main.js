50f0352003b1aa9d95e02a6553884fffb328b8b6
developer <nssm@a.recife.ifpe.edu.br>
2020-01-12 23:35:26 -0300

    Inserindo parallax, folha de estilo e ícones

--- js/main.js
+++ js/main.js
@@ -1,3 +1,26 @@
 'use strict'
-$('body').fadeOut()
-$('body').fadeIn()
+$(document).ready(() => {
+
+    $('.container-fluid').pagepiling({
+        sectionsColor: ['white', '#d8dbe2', '#a9bcd0', '#58a4b0', '#373f51', '#1b1b1e'],
+    })
+
+    document.querySelector('body').setAttribute('style', 'display: block')
+
+    document.querySelectorAll('button').forEach((item, index) => {
+        item.addEventListener('click', (item, index) => {
+            switch (index) {
+                case 0:
+                    window.open('https://www.linkedin.com/in/n2sml')
+                    break
+                case 1:
+                    window.open('mailto:nssm@a.recife.ifpe.edu.br')
+                    break
+                case 2:
+                    window.open('https://github.com/n2sml')
+                    break
+
+            }
+        })
+    })
+})
