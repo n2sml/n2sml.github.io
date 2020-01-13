b694a3e1b6cb1147570d94bf9c6e94c10f955c1e
developer <nssm@a.recife.ifpe.edu.br>
2020-01-12 23:46:40 -0300

    Ajustando Event Listeners e trecho do texto

--- js/main.js
+++ js/main.js
@@ -8,7 +8,7 @@ $(document).ready(() => {
     document.querySelector('body').setAttribute('style', 'display: block')
 
     document.querySelectorAll('button').forEach((item, index) => {
-        item.addEventListener('click', (item, index) => {
+        item.addEventListener('click', () => {
             switch (index) {
                 case 0:
                     window.open('https://www.linkedin.com/in/n2sml')
@@ -17,7 +17,7 @@ $(document).ready(() => {
                     window.open('mailto:nssm@a.recife.ifpe.edu.br')
                     break
                 case 2:
-                    window.open('https://github.com/n2sml')
+                    window.open('https://github.com/n2sml/')
                     break
 
             }
